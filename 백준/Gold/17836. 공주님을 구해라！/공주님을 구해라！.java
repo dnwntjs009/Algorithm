c06caca2200170067dc294b609a0ed682931b8f1
dnwntjs009 <149358593+dnwntjs009@users.noreply.github.com>
2025-08-04 23:23:19 +0900

    [Gold V] Title: 공주님을 구해라!, Time: 180 ms, Memory: 15736 KB -BaekjoonHub

---
+++ 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.java
@@ -0,0 +1,71 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n, m, t;
+    static int[] di = {0, 1, 0, -1};
+    static int[] dj = {1, 0, -1, 0};
+    static int[][] arr;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        t = Integer.parseInt(st.nextToken());
+
+        arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int result = bfs();
+        System.out.println(result == -1 || result > t? "Fail" : result);
+    }
+
+    static int bfs() {
+        boolean[][][] visited = new boolean[2][n][m];
+        Queue<int[]> q = new ArrayDeque<>();
+        q.add(new int[]{0, 0, 0, 0});
+        visited[0][0][0] = true;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.remove();
+            int i = cur[0], j = cur[1], cnt = cur[2], gral = cur[3];
+
+            if (i == n-1 && j == m-1) return cnt;
+
+            for (int k = 0; k < 4; k++) {
+                int ni = i + di[k], nj = j + dj[k];
+                if (0 <= ni && ni < n && 0 <= nj && nj < m) {
+                    if (arr[ni][nj] == 0) {
+                        if (gral == 0 && !visited[gral][ni][nj]) {
+                            visited[gral][ni][nj] = true;
+                            q.add(new int[]{ni, nj, cnt+1, gral});
+                        } else if (gral == 1 && !visited[gral][ni][nj]) {
+                            visited[gral][ni][nj] = true;
+                            q.add(new int[]{ni, nj, cnt+1, gral});
+                        }
+                    } else if (arr[ni][nj] == 1) {
+                        if (gral == 1 && !visited[1][ni][nj]) {
+                            visited[1][ni][nj] = true;
+                            q.add(new int[]{ni, nj, cnt+1, 1});
+                        }
+                    } else if (arr[ni][nj] == 2) {
+                        visited[0][ni][nj] = true;
+                        visited[1][ni][nj] = true;
+                        q.add(new int[]{ni, nj, cnt+1, 1});
+                    }
+                }
+            }
+        }
+        return -1;
+
+    }
+}
