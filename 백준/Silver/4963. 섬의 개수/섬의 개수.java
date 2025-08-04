4cbb9400608c6d0cbe6b7eab2801b512bce79ec2
dnwntjs009 <149358593+dnwntjs009@users.noreply.github.com>
2025-08-04 16:19:58 +0900

    [Silver II] Title: 섬의 개수, Time: 160 ms, Memory: 16048 KB -BaekjoonHub

---
+++ 백준/Silver/4963. 섬의 개수/섬의 개수.java
@@ -0,0 +1,66 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int[] di = {0, 1, 0, -1, 1, -1, -1, 1};
+    static int[] dj = {1, 0, -1, 0, 1, 1, -1, -1};
+    static boolean[][] visited;
+    static int[][] arr;
+    static int w, h;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        while (true) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            w = Integer.parseInt(st.nextToken());
+            h = Integer.parseInt(st.nextToken());
+
+            if (w == 0 && h == 0) break;
+
+            int cnt = 0;
+            arr = new int[h][w];
+            visited = new boolean[h][w];
+
+            for (int i = 0; i < h; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < w; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            for (int i = 0; i < h; i++) {
+                for (int j = 0; j < w; j++) {
+                    if (arr[i][j] == 1 && !visited[i][j]) {
+                        cnt++;
+                        bfs(i,j);
+                    }
+                }
+            }
+
+            System.out.println(cnt);
+
+        }
+    }
+
+    static void bfs(int si, int sj) {
+        Queue<int[]> q = new ArrayDeque<>();
+        q.add(new int[]{si, sj});
+        visited[si][sj] = true;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.remove();
+            int i = cur[0], j = cur[1];
+            for (int k = 0; k < 8; k++) {
+                int ni = i + di[k], nj = j + dj[k];
+                if (0 <= ni && ni < h && 0 <= nj && nj < w && arr[ni][nj] == 1 && !visited[ni][nj]) {
+                    visited[ni][nj] = true;
+                    q.add(new int[]{ni, nj});
+                }
+            }
+        }
+    }
+}
