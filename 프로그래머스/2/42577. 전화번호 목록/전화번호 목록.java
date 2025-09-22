5e4a519fad0c552d4d4c2fd5959389ef0ee5c0ed
dnwntjs009 <149358593+dnwntjs009@users.noreply.github.com>
2025-09-22 14:51:50 +0900

    [level 2] Title: 전화번호 목록, Time: 381.38 ms, Memory: 98.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        int n = phone_book.length;
+        Arrays.sort(phone_book);
+        
+        for (int i = 0; i < n-1; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i])) {
+                answer = false;
+                break;
+            }
+        }
+        return answer;
+    }
+}
